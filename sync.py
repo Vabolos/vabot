48de45cda1acabcff011baea5f67cc140399fdfb
Vabolos <79908586+Vabolos@users.noreply.github.com>
2023-10-02 21:54:38 +0200

    Create sync.py
    needed to sync the global commands, not sure if it still works tho

---
+++ sync.py
@@ -0,0 +1,45 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+from typing import Optional, Literal
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix='~', intents=intents)
+TOKEN = 'token here (of the bot you want to sync the commands of)'
+
+
+@bot.command()
+@commands.guild_only()
+@commands.is_owner()
+async def sync(
+  ctx: Context, guilds: commands.Greedy[discord.Object], spec: Optional[Literal["~", "*", "^"]] = None) -> None:
+    if not guilds:
+        if spec == "~":
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "*":
+            ctx.bot.tree.copy_global_to(guild=ctx.guild)
+            synced = await ctx.bot.tree.sync(guild=ctx.guild)
+        elif spec == "^":
+            ctx.bot.tree.clear_commands(guild=ctx.guild)
+            await ctx.bot.tree.sync(guild=ctx.guild)
+            synced = []
+        else:
+            synced = await ctx.bot.tree.sync()
+
+        await ctx.send(
+            f"Synced {len(synced)} commands {'globally' if spec is None else 'to the current guild.'}"
+        )
+        return
+
+    ret = 0
+    for guild in guilds:
+        try:
+            await ctx.bot.tree.sync(guild=guild)
+        except discord.HTTPException:
+            pass
+        else:
+            ret += 1
+
+    await ctx.send(f"Synced the tree to {ret}/{len(guilds)}.")
+
+bot.run(TOKEN)
